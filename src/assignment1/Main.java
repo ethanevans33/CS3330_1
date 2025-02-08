7e4bbd66cdb380e8753cb8e3975b03e89f395e7e
eteva <eteva@100.110.160.123>
2025-02-08 00:12:52 -0600

    The unimplemented main program

---
+++ src/assignment1/Main.java
@@ -0,0 +1,13 @@
+package assignment1;
+
+import assignment1.librarysystem.Book;
+import assignment1.librarysystem.Library;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
